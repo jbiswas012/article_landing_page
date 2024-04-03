bc120f5428a62435000cf82d64e90c898d4442e3
jbiswas012 <163222310+jbiswas012@users.noreply.github.com>
2024-04-03 12:02:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,17 +13,19 @@ async function fetchcollectionTypes(){
             throw new Error ("Network response was not ok");
         }
         const articleData = await articleResponse.json();
-        
+
         const leftsideContent = document.getElementById('leftArticleContent');
             leftsideContent.innerHTML = '';
             const articleElement = document.createElement('div');
             articleElement.innerHTML = `
             <h2>&#10012;${articleData[0].title}</h2>
             <p> ${articleData[0].intro}</p>
-            <p>&#x1F550;${articleData[0].commentcount}m <span><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16">
-            <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-          </svg>${articleData[0].commentcount}</span></p>
-            `;
+            <p>&#x1F550;${articleData[0].commentcount}m 
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16"><path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/></svg>
+                    ${articleData[0].commentcount}
+                </span>
+            </p>`;
             leftsideContent.appendChild(articleElement);
             document.getElementById('articleLgImage').src = articleData[0].imageurl;
 
@@ -34,10 +36,12 @@ async function fetchcollectionTypes(){
             articleRElement.innerHTML = `
             <h2>&#10012;${articleData[1].title}</h2>
             <p> ${articleData[1].intro}</p>
-            <p>&#x1F550;${articleData[1].published}m <span><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16">
-            <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
-          </svg>${articleData[1].commentcount}</span></p>
-            `;
+            <p>&#x1F550;${articleData[1].published}m 
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16"><path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/></svg>
+                    ${articleData[1].commentcount}
+                </span>
+            </p>`;
             rightsideContent.appendChild(articleRElement);
             document.getElementById('articleImage').src = articleData[1].imageurl;
 
@@ -47,8 +51,7 @@ async function fetchcollectionTypes(){
             const articleS1Element = document.createElement('div');
             articleS1Element.innerHTML = `
             <h4>&#10012;${articleData[2].title}</h4>
-            <p>&#x1F550;${articleData[2].published}m </p>
-            `;
+            <p>&#x1F550;${articleData[2].published}m </p>`;
             smallArt1Content.appendChild(articleS1Element);
             document.getElementById('s1articleImage').src = articleData[2].imageurl;
 
@@ -58,8 +61,7 @@ async function fetchcollectionTypes(){
             const articleS2Element = document.createElement('div');
             articleS2Element.innerHTML = `
             <h4>&#10012;${articleData[3].title}</h4>
-            <p>&#x1F550;${articleData[3].published}m </p>
-            `;
+            <p>&#x1F550;${articleData[3].published}m </p>`;
             smallArt2Content.appendChild(articleS2Element);
             document.getElementById('s2articleImage').src = articleData[3].imageurl;
     }
